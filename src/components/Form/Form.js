11f3bc9bef92ecbc027213e92530797561d42843
amandabrbz <amanda.brbz@hotmail.com>
2021-05-24 23:30:43 -0300

    add a data name attribute for testing form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,13 +2,19 @@ import './Form.scss'
 
 const Form = () => {
   return (
-    <form className="form">
+    <form className="form" id="formCharacter" data-testid="form-character">
       <input
         type="text"
         placeholder="Type to search one character..."
         className="form--input"
       />
-      <button type="submit" className="form--submit" title="click to search a character">Search</button>
+      <button
+        type="submit"
+        className="form--submit"
+        title="click to search a character"
+      >
+        Search
+      </button>
     </form>
   )
 }
